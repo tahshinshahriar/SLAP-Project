628911d257554bbefea0f8c4d22aa7074c168d0a
Samih Wadi <samihamaar@gmail.com>
2024-11-09 00:43:22 -0500

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Login from './pages/login/Login'
 
