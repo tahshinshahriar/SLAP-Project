19f749add9d1c831ab527331aba2251cc72b0df6
Samih Wadi <samihamaar@gmail.com>
2024-11-24 22:30:42 -0500

    Fixed mess

--- server/models/Group.js
+++
@@ -1,9 +0,0 @@
-const groupSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    project: { type: mongoose.Schema.Types.ObjectId, ref: 'Assignment' },
-});
-
-const Group = mongoose.model('Group', groupSchema);
-module.exports = Group;
